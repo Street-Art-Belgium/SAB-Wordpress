f046dd5a2bd5655ddb6526747dac84a2162f6ad0
Jurriaan Persyn <jurriaan@engagor.com>
2011-09-04 23:30:41 +0200

    stylesheet version bump

--- wp-content/themes/sab/header.php
+++ wp-content/themes/sab/header.php
@@ -31,7 +31,7 @@
 
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>?v=22" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>?v=23" />
 <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <link rel="icon" type="image/png" href="http://streetartbelgium.com/favicon.ico">
